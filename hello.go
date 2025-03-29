0921944b1d3681d93809ac3396eb5f829c85233c
Aroosh <kumararoosh@gmail.com>
2025-03-29 14:54:15 -0600

    Add starting functionality

---
+++ hello.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "This is my website\n");
+}
