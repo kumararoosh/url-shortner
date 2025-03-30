9877d321b08dfb8a010779196cdeaca8dbc9c49b
Aroosh <kumararoosh@gmail.com>
2025-03-30 12:12:49 -0600

    create ecr and ecs roles

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -1,3 +1,87 @@
 resource "aws_ecs_cluster" "shortener" {
   name = "shortener-cluster"
 }
+
+resource "aws_ecs_task_definition" "shortener" {
+  family                   = "shortener-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "shortener"
+      image     = "${aws_ecr_repository.shortener.repository_url}:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8080
+          protocol      = "tcp"
+        }
+      ]
+      environment = [
+        {
+          name  = "DB_HOST"
+          value = aws_db_instance.shortener.address
+        },
+        {
+          name  = "DB_PORT"
+          value = "5432"
+        },
+        {
+          name  = "DB_USER"
+          value = var.db_username
+        },
+        {
+          name  = "DB_PASSWORD"
+          value = var.db_password
+        },
+        {
+          name  = "DB_NAME"
+          value = var.db_name
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_service" "shortener" {
+  name            = "shortener-service"
+  cluster         = aws_ecs_cluster.shortener.id
+  task_definition = aws_ecs_task_definition.shortener.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets         = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+    security_groups = [aws_security_group.rds.id] # Reuse RDS group for now (but better to create app-specific SG)
+    assign_public_ip = true
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.ecs_execution_policy]
+}
