b43a50d36ebab4f43cd6f1335a9ab6e14eee4e4a
Aroosh <kumararoosh@gmail.com>
2025-03-29 19:33:31 -0600

    fix docker compose

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"math/rand"
 	"net/http"
 	"regexp"
+	"os"
 
 	_ "github.com/lib/pq"
 )
@@ -18,7 +19,18 @@ var shortcodeRegex = regexp.MustCompile(`^/[\w-]{4,}$`)
 func main() {
 	var err error
 
-	connStr := "host=localhost port=5432 user=shorty password=secret dbname=shortener sslmode=disable"
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname,
+	)
+
+
 	db, err = sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
